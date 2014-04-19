cec90b23d309f67f4b91617aa275fc68e126a0b4
Cody Short <cody95@vt.edu>
2014-04-19 12:25:00 -0700

    updated course

--- OutClass/src/edu/vt/vthacks/outclass/model/ICourse.java
+++ OutClass/src/edu/vt/vthacks/outclass/model/ICourse.java
@@ -1,5 +1,13 @@
 package edu.vt.vthacks.outclass.model;
 
 public interface ICourse {
-
+	public void setSubject(String subject);
+	public void setNumber(int number);
+	public void setTitle(String title);
+	public void setCRN(int crn);
+	
+	public String getSubject();
+	public String getNumber();
+	public String getTitle();
+	public int getCRN();
 }
