c66e0c0d7ad99d7b2cc51b972d4fe49d71dfd7c0
taylorjm <taylormattison@aol.com>
2014-04-20 06:23:21 -0400

    Wheeeeeeeeeeeee

--- OutClass/src/edu/vt/vthacks/outclass/ActivityMain.java
+++ OutClass/src/edu/vt/vthacks/outclass/ActivityMain.java
@@ -1,4 +1,11 @@
 package edu.vt.vthacks.outclass;
+import android.widget.Toast;
+import com.parse.ParseException;
+import edu.vt.vthacks.outclass.model.User;
+import android.provider.Settings.Secure;
+import com.parse.ParseObject;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
@@ -32,5 +39,22 @@ public class ActivityMain extends Activity
     public void addClass(View view) {
         Intent intent = new Intent(this, AddClassActivity.class);
         startActivity(intent);
+    }
+
+    public void info(View view) {
+        new AlertDialog.Builder(this)
+        .setTitle("App Info")
+        .setMessage("Created for VTHacks by:\nTaylor Mattison\nDavid Patuwo\nAarathi Raghuraman\nCody Short\nSteve Smith")
+        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+         })
+        .setIcon(android.R.drawable.ic_dialog_info)
+         .show();
+    }
+
+    public void settings(View view) {
+        Toast.makeText(getApplicationContext(), "Settings not implemented in the Dev Version...   :(",
+            Toast.LENGTH_LONG).show();
     }
 }
